5b7794a5ddb8b5ab731f690ec17684e1383e83dd
wanglei <lei.wang@davincimotor.com>
2021-09-27 16:46:37 +0800

    充电页面滑块优化

--- DavinciCN/AppDelegate.swift
+++ DavinciCN/AppDelegate.swift
@@ -28,5 +28,34 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         self.window?.rootViewController  = rootVC
         self.window?.makeKeyAndVisible()
     }
+    
+    // MARK: - 横竖屏相关方法
+    private var orientation: UIInterfaceOrientationMask = UIInterfaceOrientationMask.portrait
+    /// 设置屏幕是否允许旋转
+    /// - Parameters:
+    ///   - allowRoation: 是否允许旋转
+    ///   - orientation: 旋转方向
+    public func setOrientaition(allowRoation: Bool, orientation: UIInterfaceOrientationMask) {
+        self.orientation = orientation
+        if allowRoation == false {
+            self.orientation = .portrait
+            UIDevice.current.setValue(UIDeviceOrientation.portrait.rawValue, forKey: "orientation")
+        } else {
+            switch orientation {
+            case .landscapeLeft:
+                UIDevice.current.setValue(UIDeviceOrientation.landscapeLeft.rawValue, forKey: "orientation")
+                break
+            case.landscapeRight:
+                UIDevice.current.setValue(UIDeviceOrientation.landscapeRight.rawValue, forKey: "orientation")
+                break
+            default:
+                break
+            }
+        }
+    }
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        
+        return self.orientation
+    }
 }
 
