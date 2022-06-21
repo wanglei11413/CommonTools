bcedd15e759ed6fd6e04537b5a148201171e705e
wanglei <lei.wang@davincimotor.com>
2022-06-21 10:19:28 +0800

    工具类更新

--- CommonTools/Tools/ExtensionTools.swift
+++ CommonTools/Tools/ExtensionTools.swift
@@ -301,3 +301,15 @@ extension UIButton {
         }
     }
 }
+
+// MARK: - Dictionary
+// MARK: - 合并字典
+//用于字典的合并，接收的参数是一个键值对时，就可以添加到原有的字典中，并且对原有字典的重复值进行覆盖为新值，不重复则保留
+extension Dictionary {
+    mutating func merge<S>(_ other: S)
+        where S: Sequence, S.Iterator.Element == (key: Key, value: Value){
+            for (k ,v) in other {
+                self[k] = v
+        }
+    }
+}
